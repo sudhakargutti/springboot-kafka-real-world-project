953928d45da5e4b87ea8669689c45833704cd9d3
sudhakar gutti <sudhakar8324@gmail.com>
2025-05-22 01:30:14 +0530

    initial commit

---
+++ src/test/java/com/sudha/tech/springboot/kafka/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudha.tech.springboot.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
